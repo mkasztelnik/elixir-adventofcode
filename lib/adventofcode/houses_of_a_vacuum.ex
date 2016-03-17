e653e9bdfde1cb90401baa4489ed34d866c60728
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-03-17 12:59:10 +0100

    Day 3: Perfectly Spherical Houses in a Vacuum part 2

--- lib/adventofcode/houses_of_a_vacuum.ex
+++ lib/adventofcode/houses_of_a_vacuum.ex
@@ -17,22 +17,22 @@ defmodule AdventOfCode.HousesOfAVacuum do
   > delivers presents to 2 houses: one at the starting location, and one to the
   east.
 
-  iex> AdventOfCode.HousesOfAVacuum.size(">")
+  iex> AdventOfCode.HousesOfAVacuum.visited_houses(">")
   2
 
   ^>v< delivers presents to 4 houses in a square, including twice to the house
   at his starting/ending location.
 
-  iex> AdventOfCode.HousesOfAVacuum.size("^>v<")
+  iex> AdventOfCode.HousesOfAVacuum.visited_houses("^>v<")
   4
 
   ^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2
   houses.
 
-  iex> AdventOfCode.HousesOfAVacuum.size("^v^v^v^v^v")
+  iex> AdventOfCode.HousesOfAVacuum.visited_houses("^v^v^v^v^v")
   2
   """
-  def size(instructions) do
+  def visited_houses(instructions) do
     instructions
     |> String.codepoints
     |> visit_house({0, 0}, MapSet.new)
@@ -43,16 +43,74 @@ defmodule AdventOfCode.HousesOfAVacuum do
     MapSet.put(visited, house)
   end
 
-  defp visit_house(["<" | rest], {x, y} = house, visited) do
-    visit_house(rest, {x - 1, y}, MapSet.put(visited, house))
+  defp visit_house([move | rest], house, visited) do
+    visit_house(rest, next_house(move, house), MapSet.put(visited, house))
   end
-  defp visit_house([">" | rest], {x, y} = house, visited) do
-    visit_house(rest, {x + 1, y}, MapSet.put(visited, house))
+
+  defp next_house("<", {x, y}), do: {x - 1, y}
+  defp next_house(">", {x, y}), do: {x + 1, y}
+  defp next_house("^", {x, y}), do: {x, y + 1}
+  defp next_house("v", {x, y}), do: {x, y - 1}
+
+  @doc ~S"""
+  The next year, to speed up the process, Santa creates a robot version of
+  himself, Robo-Santa, to deliver presents with him.
+
+  Santa and Robo-Santa start at the same location(delivering two presents to the
+  same starting house), then take turns moving based on instructions from the
+  elf, who is eggnoggedly reading from the same script as the previous year.
+
+  This year, how many houses receive at least one present?
+
+  ## Examples
+
+  ^v delivers presents to 3 houses, because Santa goes north, and then
+  Robo-Santa goes south.
+
+  iex> AdventOfCode.HousesOfAVacuum.visited_houses_with_robot("^v")
+  3
+
+  ^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back
+  where they started.
+
+  iex> AdventOfCode.HousesOfAVacuum.visited_houses_with_robot("^>v<")
+  3
+
+  ^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction
+  and Robo-Santa going the other.)
+
+  iex> AdventOfCode.HousesOfAVacuum.visited_houses_with_robot("^v^v^v^v^v")
+  11
+
+  ^ delivers presents to 2 houses, because only Santa goes north.
+
+  iex> AdventOfCode.HousesOfAVacuum.visited_houses_with_robot("^")
+  2
+  """
+  def visited_houses_with_robot(instructions) do
+    instructions
+    |> String.codepoints
+    |> visit_house({0, 0}, {0, 0}, MapSet.new)
+    |> MapSet.size
   end
-  defp visit_house(["^" | rest], {x, y} = house, visited) do
-    visit_house(rest, {x, y + 1}, MapSet.put(visited, house))
+
+  defp visit_house([], santa_house, robot_house, visited) do
+    add_visited(santa_house, robot_house, visited)
   end
-  defp visit_house(["v" | rest], {x, y} = house, visited) do
-    visit_house(rest, {x, y - 1}, MapSet.put(visited, house))
+
+  defp visit_house([santa], santa_house, robot_house, visited) do
+    visit_house([santa], santa_house, MapSet.put(visited, robot_house))
+  end
+
+  defp visit_house([santa, robot | rest], santa_house, robot_house, visited) do
+    visit_house(rest, next_house(santa, santa_house),
+                next_house(robot, robot_house),
+                add_visited(santa_house, robot_house, visited))
+  end
+
+  defp add_visited(santa_house, robot_house, visited) do
+    visited
+    |> MapSet.put(santa_house)
+    |> MapSet.put(robot_house)
   end
 end
